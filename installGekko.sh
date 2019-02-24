1d20dcbbc44be78900bc15bf768a84331cd34b62
cryptoStrategy <47935900+cryptoStrategy@users.noreply.github.com>
2019-02-24 09:44:38 +0100

    Update installGekko.sh

--- installGekko.sh
+++ installGekko.sh
@@ -3,10 +3,8 @@ clear
 echo "-------------------------------------------"
 echo 'INSTALLING GEKKO + DEPENDECIES'
 echo '-------------------------------------------'
-echo 'This will about 15-45 minutes, depending on your system and network connection'
+echo 'This will about 1-5 minutes, depending on your system and network connection'
 cd /mnt/c/
-mkdir Gekko-Bot
-cd Gekko-Bot
 git clone git://github.com/askmike/gekko.git
 cd gekko
 npm install --only=production
